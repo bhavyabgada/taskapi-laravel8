8b0fa62f86464f0924b4f309b44eeb3d73c92a79
bhavyabgada <bhavyabgada@gmail.com>
2021-07-22 18:46:53 +0530

    task initial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -87,7 +87,7 @@
 			type:"POST",
 			headers:{
 				"Accept":"application/json",
-				"Authorization": "Bearer {{ $token }}"
+				"Authorization":"Bearer {{ $token }}",
 			},
 			data:{
 				// "_token": "{{ csrf_token() }}",
