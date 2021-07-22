49ad746e44d96404d1212fe53f5f3fa20eea2f78
bhavyabgada <bhavyabgada@gmail.com>
2021-07-22 14:56:28 +0530

    task initial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,25 @@
 
 @section('body')
 <div class="container">
+	@if (Route::has('login'))
+	<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+		@auth
+		<a href="{{ route('logout') }}" class="text-sm text-gray-700 underline">Log out</a>
+		@else
+		<a href="{{ route('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+		<a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 underline">Log in </a>
+		@if (Route::has('register'))
+		<a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+		@endif
+		@endauth
+	</div>
+	@endif
 	<div class="row">
 		<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
 			<div class="card card-signin my-5">
 				<div class="card-body">
 					<h5 class="card-title text-center">Task List</h5>
-					{{ $tasks = Session::get('tasks') }}
-					@foreach ($tasks as $task)
+					@foreach (Session::get('tasks') as $task)
 					<table class="table table-hover">
 						<thead>
 							<tr>
