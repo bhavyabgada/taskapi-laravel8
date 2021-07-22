2cc4009bbb1962bc376b199819190f5346b08402
bhavyabgada <bhavyabgada@gmail.com>
2021-07-22 19:02:10 +0530

    task initial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -82,43 +82,42 @@
 		let task = $('#task').val();
 		let status = ($("#done").prop("checked") == true ? 'Done' : 'Pending');
 
-		// $.ajax({
-		// 	url: "/api/task/add",
-		// 	type:"POST",
-		// 	headers:{
-		// 		"Accept":"application/json",
-		// 		"Authorization":"Bearer {{ $token }}",
-		// 	},
-		// 	data:{
-		// 		// "_token": "{{ csrf_token() }}",
-		// 		task:task,
-		// 		status:status,
-		// 	},
-		// 	success:function(response){
-		// 		console.log(response);
-		// 		window.location = {{ route('dashboard') }}
-		// 	},
-		// });
-		var url = "http://laravel8-auth.herokuapp.com/api/task/add";
-
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url);
-
-		xhr.setRequestHeader("Accept", "application/json");
-		xhr.setRequestHeader("Authorization", "Bearer {{ $token }}");
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === 4) {
-				console.log(xhr.status);
-				console.log(xhr.responseText);
-			}};
-
-			var data = "task="+task+"&status="+status;
-
-			xhr.send(data);
-			window.location.reload();
+		$.ajax({
+			url: "/api/task/add",
+			type:"POST",
+			headers:{
+				"Accept":"application/json",
+				"Authorization":"Bearer {{ $token }}",
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			data:{
+				// "_token": "{{ csrf_token() }}",
+				task:task,
+				status:status,
+			},
+			success:function(response){
+				console.log(response);
+				window.location = {{ route('dashboard') }}
+			},
 		});
-	// window.location.reload();
-</script>
-@endsection
+		// var url = "http://laravel8-auth.herokuapp.com/api/task/add";
+
+		// var xhr = new XMLHttpRequest();
+		// xhr.open("POST", url);
+
+		// xhr.setRequestHeader("Accept", "application/json");
+		// xhr.setRequestHeader("Authorization", "Bearer {{ $token }}");
+		// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		// xhr.onreadystatechange = function () {
+		// 	if (xhr.readyState === 4) {
+		// 		console.log(xhr.status);
+		// 		console.log(xhr.responseText);
+		// 	}};
+
+		// 	var data = "task="+task+"&status="+status;
+
+		// 	xhr.send(data);
+		// });
+	</script>
+	@endsection
