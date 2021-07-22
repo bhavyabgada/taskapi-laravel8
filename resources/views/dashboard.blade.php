14c53e9d955c6217c4bb1c4ba61fbd4bee12f8cc
bhavyabgada <bhavyabgada@gmail.com>
2021-07-22 14:50:18 +0530

    task initial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,33 +7,21 @@
 			<div class="card card-signin my-5">
 				<div class="card-body">
 					<h5 class="card-title text-center">Task List</h5>
-					@foreach ($tasks as $task)
+					{{ $tasks = Session::get('tasks') }}
+					@foreach (tasks as $task)
 					<table class="table table-hover">
 						<thead>
 							<tr>
 								<th scope="col">#</th>
-								<th scope="col">First</th>
-								<th scope="col">Last</th>
-								<th scope="col">Handle</th>
+								<th scope="col">Task</th>
+								<th scope="col">Status</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row">1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-								<td>@mdo</td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Jacob</td>
-								<td>Thornton</td>
-								<td>@fat</td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td colspan="2">Larry the Bird</td>
-								<td>@twitter</td>
+								<th scope="row">{{ $task->id }}</th>
+								<td>{{ $task->task }}</td>
+								<td>{{ $task->status }}</td>
 							</tr>
 						</tbody>
 					</table>
